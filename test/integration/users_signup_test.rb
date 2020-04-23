9bd6b7666f914ef2b1a420d2fa15ef70c14f8761
weixiangtoh <63725862+weixiangtoh@users.noreply.github.com>
2020-04-23 10:46:44 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.nil?
+    assert is_logged_in?
+
   end
   
 end
