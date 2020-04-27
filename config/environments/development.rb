8af51a3ff1be7a26775c4a530dd3aabce22a5eff
weixiangtoh <63725862+weixiangtoh@users.noreply.github.com>
2020-04-27 09:45:05 +0000

    Finish static pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,12 +29,12 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = 'example.com' # Don't use this literally; use your local dev host instead
+  host = 'radiant-sierra-39624.herokuapp.com' # Don't use this literally; use your local dev host instead
   # Use this on the cloud IDE.
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Use this if developing on localhost.
