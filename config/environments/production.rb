9a7cf2c560de891fdec3f9736b21f38c39b54751
weixiangtoh <63725862+weixiangtoh@users.noreply.github.com>
2020-04-27 10:15:14 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,13 +71,27 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'radiant-sierra-39624.herokuapp.com',
     :enable_starttls_auto => true
   }
+  
+  
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'example.com',
+#   user_name:            '<username>',
+#   password:             '<password>',
+#   authentication:       'plain',
+#   enable_starttls_auto: true }
+  
+  
+  
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
